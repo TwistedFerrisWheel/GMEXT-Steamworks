e2b2f029ce638c2c69d7fc9f1f99383ce340fd7b
Aaron <Aaron1997ps@gmail.com>
2024-12-28 19:50:45 -0600

    Amaranth Test

---
+++ source/Steamworks_gml/extensions/steamworks/steamworks_cpp/GMLSteam/amaranth_test.cpp
@@ -0,0 +1,17 @@
+/// amaranth_test.cpp
+
+#include "pch.h"
+#include "steam_glue.h"
+#include "steam_api.h"
+#include "Extension_Interface.h"
+#include "YYRValue.h"
+#include "steam_common.h"
+
+YYEXPORT void /*double*/ amaranth_add(RValue& Result, CInstance* selfinst, CInstance* otherinst, int argc, RValue* arg)//(double auto_accept)
+{
+    double firstNum = YYGetReal(arg, 0);
+    double secondNum = YYGetReal(arg, 1);
+
+    Result.kind = VALUE_REAL;
+    Result.val = firstNum+secondNum;
+}
